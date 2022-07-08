41d2b7a063732c4c0dcf19446dc631be1754f2dc
AliakbarETH <{ID}+{username}@users.noreply.github.com>
2022-07-08 06:05:11 +0500

    Day 9

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -5,8 +5,23 @@ class MyDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final imageURL = "https://media-exp1.licdn.com/dms/image/C4D03AQERC5LYNO1tKA/profile-displayphoto-shrink_200_200/0/1651912172990?e=2147483647&v=beta&t=A29uqryy6NoBPsvY6lj0EAH5cP86Jcv_pmy50RdX6mk";
+
+
     return Drawer( 
-      child: ListView(),
+      child: ListView(
+        children: [
+          DrawerHeader(
+            padding: EdgeInsets.zero,
+            margin: EdgeInsets.zero,
+            child: UserAccountsDrawerHeader(
+            accountName: Text("Ali Akbar")
+            accountEmail: Text("abc@gmail.com"),
+            currentAccountPicture: Image.network(imageURL),
+          ))
+        ],
+      ),
 
     );
   }
